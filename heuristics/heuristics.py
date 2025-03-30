61c6201fba2138c847a2a4a0687dd1424da843fe
Jaume <jaumesansacolo@gmail.com>
2025-03-30 19:02:35 +0100

    coment time heuristics

--- heuristics/heuristics.py
+++ heuristics/heuristics.py
@@ -35,43 +35,43 @@ def h2(board):
 
 # Calculate execution time to extract it from the total execution of the program.
 
-num_tests = 10  # Número de pruebas por nivel
-levels = [1, 2, 3]  # Niveles a probar
+# num_tests = 10  # Número de pruebas por nivel
+# levels = [1, 2, 3]  # Niveles a probar
 
-total_time_h1 = 0
-total_time_h2 = 0
-total_tests = 0
+# total_time_h1 = 0
+# total_time_h2 = 0
+# total_tests = 0
 
-for level in levels:
-    level_time_h1 = 0
-    level_time_h2 = 0
+# for level in levels:
+#     level_time_h1 = 0
+#     level_time_h2 = 0
 
-    for _ in range(num_tests):
-        board = generate_board(level)
+#     for _ in range(num_tests):
+#         board = generate_board(level)
 
-        start = time.perf_counter()
-        h1(board)
-        end = time.perf_counter()
-        level_time_h1 += (end - start)
+#         start = time.perf_counter()
+#         h1(board)
+#         end = time.perf_counter()
+#         level_time_h1 += (end - start)
 
-        start = time.perf_counter()
-        h2(board)
-        end = time.perf_counter()
-        level_time_h2 += (end - start)
+#         start = time.perf_counter()
+#         h2(board)
+#         end = time.perf_counter()
+#         level_time_h2 += (end - start)
 
-    avg_h1 = level_time_h1 / num_tests
-    avg_h2 = level_time_h2 / num_tests
+#     avg_h1 = level_time_h1 / num_tests
+#     avg_h2 = level_time_h2 / num_tests
 
-    print(f"Level {level} - Average TIME h1: {avg_h1:.10f} sec")
-    print(f"Level {level} - Average TIME h2: {avg_h2:.10f} sec")
+#     print(f"Level {level} - Average TIME h1: {avg_h1:.10f} sec")
+#     print(f"Level {level} - Average TIME h2: {avg_h2:.10f} sec")
 
-    total_time_h1 += level_time_h1
-    total_time_h2 += level_time_h2
-    total_tests += num_tests
+#     total_time_h1 += level_time_h1
+#     total_time_h2 += level_time_h2
+#     total_tests += num_tests
 
-# Cálculo de la media global
-global_avg_h1 = total_time_h1 / total_tests
-global_avg_h2 = total_time_h2 / total_tests
+# # Cálculo de la media global
+# global_avg_h1 = total_time_h1 / total_tests
+# global_avg_h2 = total_time_h2 / total_tests
 
-print(f"Global Average TIME h1: {global_avg_h1:.10f} sec")
-print(f"Global Average TIME h2: {global_avg_h2:.10f} sec")
+# print(f"Global Average TIME h1: {global_avg_h1:.10f} sec")
+# print(f"Global Average TIME h2: {global_avg_h2:.10f} sec")
